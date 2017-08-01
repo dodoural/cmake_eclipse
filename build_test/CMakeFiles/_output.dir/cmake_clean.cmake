daa89f690b11da26c89a3b4c2a24302e0b5aaf69
dogusural <dogusural@gmail.com>
2017-08-01 15:44:23 +0300

    Add files via upload

---
+++ build_test/CMakeFiles/_output.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/_output.dir/src/class/test.cpp.o"
+  "CMakeFiles/_output.dir/src/main/main.cpp.o"
+  "_output.pdb"
+  "_output"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/_output.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/class/test.hpp
@@ -0,0 +1,11 @@
+#ifndef TEST_HPP
+#define TEST_HPP
+#include <iostream>
+
+class Rectangle {
+    int width, height;
+  public:
+    void set_values (int,int);
+    int area();
+};
+#endif
